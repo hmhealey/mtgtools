7ada891f58221f02f6d3356952f3abfb4ac376bb
Harrison Healey <harrisonmhealey@gmail.com>
2023-07-19 18:09:29 -0400

    Add test for incorrect case involving anchor words

--- app/src/components/oracle_text.tsx
+++ app/src/components/oracle_text.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo} from 'react';
+import React, {ReactNode, useMemo} from 'react';
 
 import {Card} from '@hmhealey/scryfall/types/card';
 
@@ -13,31 +13,74 @@ export interface Props {
 export default function OracleText(props: Props) {
     const oracleText = props.card.oracle_text;
 
-    const rendered = useMemo(() => {
-        const parts = [];
-
-        let remaining = oracleText;
-        while (remaining) {
-            let match = /^\{[^}]*\}/.exec(remaining);
-            if (match) {
-                parts.push(
-                    <ManaSymbol
-                        key={parts.length}
-                        symbol={match[0]}
-                    />,
-                );
-                remaining = remaining.substring(match[0].length);
-                continue;
-            }
-
-            match = /^.[^{]*/.exec(remaining);
-
-            parts.push(match[0]);
-            remaining = remaining.substring(match[0].length);
-        }
-
-        return parts;
-    }, [oracleText]);
+    const rendered = useMemo(() => renderOracleText(oracleText), [oracleText]);
 
     return <div className={classNames('OracleText', props.className)}>{rendered}</div>;
 }
+
+// const parts: ReactNode[] = [];
+
+// let remaining = text;
+// while (remaining) {
+let match = /^\{[^}]*\}/.exec(remaining);
+if (match) {
+    parts.push(
+        <ManaSymbol
+            key={parts.length}
+            symbol={match[0]}
+        />,
+    );
+    remaining = remaining.substring(match[0].length);
+    continue;
+}
+
+//     if (!inReminderText) {
+//         match = /^\([^)]*\)/.exec(remaining);
+//         if (match) {
+//             parts.push(<i key={parts.length}>{match[0]}</i>);
+//             remaining = remaining.substring(match[0].length);
+//             continue;
+//         }
+
+//         match = /^\n/.exec(remaining);
+
+//     }
+
+match = /^.+(?=[{(]|$)/.exec(remaining);
+//     console.log('matched plain text aaa', remaining, match);
+
+//     parts.push(match[0]);
+//     remaining = remaining.substring(match[0].length);
+// }
+
+// console.log(text);
+
+// return parts;
+
+function renderOracleText(text: string): ReactNode[] {
+    return [];
+}
+
+// function parseManaSymbols(text: string) {
+//     const parts: ReactNode[] = [];
+
+//     let remaining = text;
+//     while (remaining) {
+//         let match = matchManaSymbol(remaining);
+//         if ()
+//     }
+
+//     return parts;
+// }
+
+// function matchManaSymbol(text: string) {
+//     let match = /^\{[^}]*\}/.exec(text);
+//     if (match) {
+//         return {
+//             symbol: match[0],
+//             remaining: text.substring(match[0].length),
+//         };
+//     }
+
+//     return null;
+// }
